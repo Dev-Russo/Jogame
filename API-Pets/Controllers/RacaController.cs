5d54e69d457a5679bad6d5fbed9cb54f52c43ee9
Dev-Russo <murilorusso10@gmail.com>
2020-10-27 14:47:10 -0300

    projeto finalizado

--- API-Pets/Controllers/RacaController.cs
+++ API-Pets/Controllers/RacaController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API_Pets.Domains;
+using API_Pets.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,43 @@ namespace API_Pets.Controllers
     [ApiController]
     public class RacaController : ControllerBase
     {
+        //chamando repository
+        RacaRepository repo = new RacaRepository();
+
+        // GET: api/<RacaController>
+        [HttpGet]
+        public List<Raca> Get()
+        {
+            return repo.LerTodos();
+        }
+
+        // GET api/<RacaController>/5
+        [HttpGet("{id}")]
+        public Raca Get(int id)
+        {
+            return repo.BuscarPorId(id);
+        }
+
+        // POST api/<RacaController>
+        [HttpPost]
+        public void Post([FromBody] Raca r)
+        {
+            repo.Cadastrar(r);
+        }
+
+        // PUT api/<RacaController>/5
+        [HttpPut("{id}")]
+        public Raca Put(int id, [FromBody] Raca r)
+        {
+            return repo.Alterar(r);
+        }
+
+        // DELETE api/<RacaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repo.Excluir(id);
+        }
     }
 }
+
