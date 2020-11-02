4804718f35b4f0ea3eb78f2784bb5a499cdbc9d8
Dev-Russo <murilorusso10@gmail.com>
2020-11-02 17:33:01 -0300

    Projeto Jogame Finalizado

---
+++ Interfaces/IJogoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Jogame.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jogame.Interfaces
+{
+    interface IJogoRepository
+    {
+        List<Jogo> Listar();
+        Jogo BuscarPorId(Guid id);
+        List<Jogo> BuscarPorNome(string nome);
+        Jogo Adicionar(List<JogoJogadores> jogoJogadores);
+        void Editar(Jogo jogo);
+        void Remover(Guid id);
+
+    }
+}
